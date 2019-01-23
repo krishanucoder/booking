04cb6419ab6cd6342417027247133d0cb9ed94ed
Krishanu Das <krishanucoder.das@gmail.com>
2019-01-23 16:35:56 +0530

    Initial commit

---
+++ src/main/java/com/stays/bookingserver/BookingServerNewApplication.java
@@ -0,0 +1,14 @@
+package com.stays.bookingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServerNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServerNewApplication.class, args);
+	}
+
+}
+
